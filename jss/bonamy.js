62d3d992370d26abb3a659497d6585ccf4471933
Antoine  GIRARD <sapk@sapk.fr>
2013-04-04 19:03:12 +0200

    Ajout du framework swiper qui permet la transiotn entre les slides.

--- jss/bonamy.js
+++ jss/bonamy.js
@@ -1,5 +1,15 @@
 var b = {
 	"init" : function(){
-		navigator.splashscreen.hide();
+		b.swiper = new Swiper('.swiper-container', {
+		//	"loop" : true,
+			"slidesPerSlide" : parseInt($('.swiper-container').width()/500)+1,
+		});
+		$(window).resize(function() {
+			b.swiper.params.slidesPerSlide=parseInt($('.swiper-container').width()/500)+1;
+			b.swiper.reInit() 
+			console.log(b.swiper.params)
+		});
+		
+		//navigator.splashscreen.hide();
 	}
 };
