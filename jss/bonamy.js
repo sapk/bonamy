192dcb556bc1b12ac25db18c4e4f1cc98be3c9af
Antoine  GIRARD <sapk@sapk.fr>
2013-04-04 19:24:48 +0200

    Petit tweak de design

--- jss/bonamy.js
+++ jss/bonamy.js
@@ -1,7 +1,9 @@
 var b = {
 	"init" : function(){
 		b.swiper = new Swiper('.swiper-container', {
-		//	"loop" : true,
+			"loop" : true,
+			speed:400, 
+			//ratio: 0.9,
 			"slidesPerSlide" : parseInt($('.swiper-container').width()/500)+1,
 		});
 		$(window).resize(function() {
