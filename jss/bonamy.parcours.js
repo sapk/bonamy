a129d0d16c8ca4cc7af9a20f428ac4f9955e08b1
Antoine GIRARD <sapk@sapk.fr>
2013-04-13 00:45:58 +0200

    Ajout page de test pour map

--- jss/bonamy.parcours.js
+++ jss/bonamy.parcours.js
@@ -6,5 +6,23 @@ b.parcours = {
 		
 		if(!navigator)
 			navigator.splashscreen.hide();
+		$('#map').height($(document).height()-180);
+		
+		var map = L.map('map', {
+			center: [47.219401, -1.543601],
+			zoom: 17
+		});
+		/*
+		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+			attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+		}).addTo(map);
+		*/
+		//92634
+		//http://b.tile.cloudmade.com/8ee2a50541944fb9bcedded5165f09d9/1@2x/256/15/17599/10746.png
+		L.tileLayer('http://{s}.tile.cloudmade.com/{key}/{styleId}/256/{z}/{x}/{y}.png', {
+			key: '8ee2a50541944fb9bcedded5165f09d9',
+			styleId: 1
+		}).addTo(map);
+		
 	}
 };
