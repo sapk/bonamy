064bdff3ea547aec61ebbc28c242e4043210651c
Antoine GIRARD <sapk@sapk.fr>
2013-04-13 01:17:29 +0200

    Ajout tu pointeur sur la carte

--- jss/bonamy.parcours.js
+++ jss/bonamy.parcours.js
@@ -8,7 +8,7 @@ b.parcours = {
 			navigator.splashscreen.hide();
 		$('#map').height($(document).height()-180);
 		
-		var map = L.map('map', {
+		b.parcours.map = L.map('map', {
 			center: [47.219401, -1.543601],
 			zoom: 17
 		});
@@ -22,7 +22,32 @@ b.parcours = {
 		L.tileLayer('http://{s}.tile.cloudmade.com/{key}/{styleId}/256/{z}/{x}/{y}.png', {
 			key: '8ee2a50541944fb9bcedded5165f09d9',
 			styleId: 1
-		}).addTo(map);
+		}).addTo(b.parcours.map);
 		
-	}
+		b.parcours.marker = L.marker([47.219401, -1.543601],{icon:  L.icon({
+				iconUrl: '../../../img/dora.png',
+				iconSize: [50, 50],
+				iconAnchor: [25, 50]
+			})});
+			
+		b.parcours.watchID = navigator.geolocation.watchPosition(
+			b.parcours.onSuccess, b.parcours.onError, 
+			{ timeout: 30000 });
+			
+		b.parcours.marker.addTo(map);
+	},
+	
+	
+	onSuccess : function (position) {
+		b.parcours.marker.setLatLng([position.coords.latitude,position.coords.longitude])
+		b.parcours.marker.setView([position.coords.latitude,position.coords.longitude], 17)
+	},
+
+onError : function (error) {
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+}
+
+	
+	
 };
